0f7bfd2076602cb50e69407dddca7c80b3d8bb49
Angel Mendez <amendez1988@gmail.com>
2017-06-20 18:50:06 -0500

    Se elimina modulo changelogwidget de tal forma que se mantenga solo el codigo minimo necesario; el modulo es de ejemplo

--- app/src/main/java/backbase/com/templatedrawer/BackbaseApplication.java
+++ app/src/main/java/backbase/com/templatedrawer/BackbaseApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.backbase.android.Backbase;
 import com.backbase.android.rendering.BBRenderer;
 
-import backbase.com.widget.changelog.views.ChangelogWidget;
 
 public class BackbaseApplication extends Application {
 
@@ -16,7 +15,6 @@ public class BackbaseApplication extends Application {
         super.onCreate();
         Backbase.setLogLevel(Backbase.LogLevel.DEBUG);
         Backbase.initialize(this, configFilePath, false);
-        BBRenderer.registerRenderer(ChangelogWidget.class);
         BBRenderer.registerRenderer(SimplePage.class);
 
     }
--- changelogwidget/src/main/java/backbase/com/widget/changelog/views/ChangelogContract.java
+++
@@ -1,8 +0,0 @@
-package backbase.com.widget.changelog.views;
-
-import com.backbase.android.rendering.android.NativeContract;
-
-public interface ChangelogContract extends NativeContract {
-    void retrieveChangelogItems();
-    void didSelectItemAtPosition(int position);
-}
